ac5db8cde3bb262c6af54a139261e7c2d89166ae
ManuKute <emanuelkute@gmail.com>
2020-06-02 05:25:50 -0500

    updates

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,2 @@
-# OpenWeatherMap API Key
-weather_api_key = "d70c88891f4b68d27e9dc85596a2b523"
-
 # Google API Key
-g_key = "AIzaSyCB3P__HGyhiMn8wuQoU5IufnriEYW7WYk"
+g_key = "YOUR KEY HERE!"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,4 @@
 # OpenWeatherMap API Key
-weather_api_key = "d70c88891f4b68d27e9dc85596a2b523"
+weather_api_key = "YOUR KEY HERE!"
 
-# Google API Key
-g_key = "AIzaSyCB3P__HGyhiMn8wuQoU5IufnriEYW7WYk"
+
