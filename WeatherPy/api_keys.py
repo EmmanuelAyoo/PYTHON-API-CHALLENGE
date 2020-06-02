3f850471d130db2d6a7b96ab5de4d6e38a84fcca
ManuKute <emanuelkute@gmail.com>
2020-06-02 05:01:33 -0500

    homework updates

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d70c88891f4b68d27e9dc85596a2b523"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCB3P__HGyhiMn8wuQoU5IufnriEYW7WYk"
